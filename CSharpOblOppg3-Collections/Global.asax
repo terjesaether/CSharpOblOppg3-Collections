88ec65df6a797f7ec4e401f7a724a3db70100852
terje <terje@MBP-WIN>
2016-10-19 20:16:45 +0200

    Add project files.

---
+++ CSharpOblOppg3-Collections/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CSharpOblOppg3_Collections.MvcApplication" Language="C#" %>
---
+++ CSharpOblOppg3-Collections/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSharpOblOppg3_Collections.Startup))]
+namespace CSharpOblOppg3_Collections
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
